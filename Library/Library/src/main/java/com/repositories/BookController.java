4b59f99e5f8384652fc15398751e644c26468f37
Danieldiaxf <danieldias0312@gmail.com>
2025-11-17 17:02:24 -0300

    Feat: Classes Organized

--- Library/Library/src/main/java/com/repositories/BookController.java
+++ Library/Library/src/main/java/com/repositories/BookController.java
@@ -2,7 +2,6 @@ package com.repositories;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.entities.Book;
 
 @RestController
@@ -25,7 +23,7 @@ public class BookController {
 
 	@PostMapping
 	public Book saveBook( @RequestBody Book book ) {
-		return bookRepository.save(book);
+		return bookRepository.save( book );
 	}
 
 	@GetMapping
