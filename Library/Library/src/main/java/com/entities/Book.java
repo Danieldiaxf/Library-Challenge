129798a7aaa6a62ea114ba2aa4f0253776da2ead
Danieldiaxf <danieldias0312@gmail.com>
2025-11-17 13:32:12 -0300

    feat: adicionar entidades iniciais

---
+++ Library/Library/src/main/java/com/entities/Book.java
@@ -0,0 +1,56 @@
+package com.entities;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+
+    private Long id;
+    private String name;
+    private String isbn;
+    private Long year;
+    private String language;
+
+    public String getName( ) {
+        return name;
+    }
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+    public Long getYear() {
+        return year;
+    }
+    public void setYear( Long year ) {
+        this.year = year;
+    }
+
+
+    public String getLanguage(  ) {
+        return language;
+    }
+    public void setLanguage( String language ) {
+        this.language = language;
+    }
+
+    
+
+}
