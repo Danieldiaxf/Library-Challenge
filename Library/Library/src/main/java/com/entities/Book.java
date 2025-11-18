2cf3bca50e1ea83c8022019518c397a229cc0781
Danieldiaxf <danieldias0312@gmail.com>
2025-11-18 10:09:17 -0300

    Feat: Book DDL

--- Library/Library/src/main/java/com/entities/Book.java
+++ Library/Library/src/main/java/com/entities/Book.java
@@ -1,6 +1,7 @@
 package com.entities;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,6 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table( name = "Book" )
@@ -15,11 +18,17 @@ public class Book {
 
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
-
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @Column( unique = true, name = "ISBN" )
     private String isbn;
+
+    @Size( min = 4, max = 4 )
     private Long year;
+
     private String language;
 
     public String getName( ) {
