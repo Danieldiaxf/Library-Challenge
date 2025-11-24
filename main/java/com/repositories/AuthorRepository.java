cb797f84d7cfbbbc59e08b59ddbfa3a6c77da559
Danieldiaxf <danieldias0312@gmail.com>
2025-11-24 08:50:51 -0300

    Feat: Final Organization

--- main/java/com/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.entities.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Long>{
-
-}
--- main/java/com/repositories/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.entities.Publisher;
-
-public interface PublisherRepository extends JpaRepository<Publisher, Long>{
-
-
-
-}
